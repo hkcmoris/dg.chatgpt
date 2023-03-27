71d489fe69156667c09030e0d8a03301461f674f
Ondřej Moravec <moravec@devground.cz>
2023-03-27 10:01:42 +0200

    get working version

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import openai
 import os
 import logging
 import json
+from dotenv import load_dotenv
+
 
 OPENAI_API_KEY = ""
 INTRO_PROMPT = {}
@@ -10,7 +12,7 @@ INTRO_PROMPT = {}
 def setup():
     """Setup the OpenAI API key from the environment variable OPENAI_API_KEY."""
 
-    os.load_dotenv()
+    load_dotenv()
 
     if not 'OPENAI_API_KEY' in os.environ:
         warning = 'No OpenAI API key found. Set the `OPENAI_API_KEY` environment variable.'
@@ -27,7 +29,7 @@ def setup():
         logging.warning(warning)
         raise ValueError(warning)
 
-    if not os.path.exists("cofig.json"):
+    if not os.path.isfile("config.json"):
         warning = 'No config.json file found. Please create one.'
         logging.warning(warning)
         raise ValueError(warning)
@@ -53,8 +55,8 @@ def setup():
         INTRO_PROMPT = config["intro_prompt"]
 
     if not os.path.exists("chat_logs"):
-        logging.log(logging.INFO, "Creating `chats` directory")
-        os.mkdir("chats")
+        logging.log(logging.INFO, "Creating `chat_logs` directory")
+        os.mkdir("chat_logs")
 
     logging.log(logging.INFO, "OpenAI API key loaded successfully")
 
