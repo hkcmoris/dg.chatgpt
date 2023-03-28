000b12aed955d71e7d7aef0afa6b201717ae19b7
Ondřej Moravec <moravec@devground.cz>
2023-03-28 08:39:45 +0200

    add packages to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='DevGround.cz - chatGPT module',
     version='1.0',
     scripts=['chatgpt.py'],
+    packages=find_packages()
 )
