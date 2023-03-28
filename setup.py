7b0ba4e5c674e847ce111cd5a3960bf7a4404a83
Ondřej Moravec <moravec@devground.cz>
2023-03-28 12:26:29 +0200

    update file structure and setup

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='dg_chatgpt',
     version='1.0',
+    description='wrapper for OpenAI chatgpt API, using `gpt-3.5-turbo` model',
+    url='https://github.com/hkcmoris/dg_chatgpt',
+    author='Ondřej Moravec',
+    author_email='moravec@devground.cz',
     packages=find_packages(),
     install_requires=[
         'openai',
